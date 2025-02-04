68252575c6b221d1e011520fbe8073adaab0a49d
oprailTodoTest <calvinfan9@gmail.com>
2025-02-05 00:04:25 +0530

    update -> simple todo app

--- all-projects/simple-todo-list/todo.js
+++ all-projects/simple-todo-list/todo.js
@@ -1,111 +1,71 @@
+
+
+
+
+
+
 const addNewFolderBtn = document.querySelector("#add-new-item-btn");
 
-const newFolderWindow = document.querySelector(".create-new-folder");
 const newTodoWindow = document.querySelector(".create-new-todo");
+
 const popupBackground = document.querySelector("#background-hide");
 
 // New Folder & to-do popup acess
 const userInputColor = document.querySelector("#user-color");
-const userInputFolderName = document.querySelector("#user-fol-name");
 const userInputTodo = document.querySelector("#user-todo");
 
 // popup btns 
-const popupCloseBtn = document.querySelector("#cancel-btn");
-const popupAddBtn = document.querySelector("#add-btn");
 const todoCloseBtn = document.querySelector("#todo-cancel-btn")
 const todoAddBtn = document.querySelector("#todo-add-btn");
-const goBackBtn = document.querySelector("#goBackBtn");
 
 // variables for page
-const folderPage = document.querySelector(".folder-page");
 const todoPage = document.querySelector(".todo-page");
 
-const headerBack = document.querySelector("#header-back");
+const allTodos = document.querySelector(".all-todo-container");
+
+
+
 const headerFolder = document.querySelector("#header-folder");
+// variable
+let todoData;
+
+
+
+// on page load 
+displaySavedData();
+
+
+
+
+
 
 
 
 //  all functions =>
-// open new folder popup/window
-function openNewFolderWindow() {
-  
-   let btnClass = addNewFolderBtn.getAttribute("class");
-   
-   if (btnClass === "add-new-folder") {
-    newFolderWindow.style.display = "inline-block";
-    popupBackground.style.display = "block";
-   } else {
+// open new to-do popup/window
+function openTodoPopup() {
     newTodoWindow.style.display = "inline-block";
     popupBackground.style.display = "block";
-    }
 }
 
 // close new folder popup/window
-function closeNewFolderWindow() {
-   newFolderWindow.style.display = "none";
+function closeTodoPopup() {
    popupBackground.style.display = "none";
    newTodoWindow.style.display = "none";
     
     userInputColor.value = "";
-    userInputFolderName.value = "";
+    userInputTodo.value = "";
     userInputTodo.value = "";
 }
 
-// add new folder/list =>
-function addNewFolder() {
-   // create elements
-    let newFolder = document.createElement("div");
-    let newFolderItemColor = document.createElement("div");
-    let newFolderI = document.createElement("i");
-    let newFolderP = document.createElement("p");
-    
-    // add class attributes
-    newFolder.setAttribute("class","folder");
-    newFolderItemColor.setAttribute("class","item-color");
-    newFolderI.setAttribute("class","fas fa-folder");
-    newFolderP.setAttribute("id","folderName");
-    
-    // asign values  
-    newFolderP.innerText = userInputFolderName.value;
-    newFolderItemColor.style.background = userInputColor.value; 
-    
-    // append in dom
-    newFolder.append(newFolderItemColor);
-    newFolder.append(newFolderI);
-    newFolder.append(newFolderP);
-    folderPage.append(newFolder);
-   
-   closeNewFolderWindow();
-}
-
-// open to-do of folder/list
-function openTodoPage(event) {
-   
-   const folderName = document.querySelector("#folder-name");
-   folderName.innerText = event.target.innerText;
-   
-   headerBack.style.display = "flex";
-   headerFolder.style.display = "none";
-   folderPage.style.display = "none";
-   todoPage.style.display = "inline";
-   
-   addNewFolderBtn.setAttribute("class","add-new-todo");
-}
-
-// close to-do page go back in lists
-function closeTodoPage() {
-   headerBack.style.display = "none";
-   headerFolder.style.display = "block";
-   folderPage.style.display = "inline";
-   todoPage.style.display = "none";
-   addNewFolderBtn.setAttribute("class",
-   "add-new-folder");
-}
 
 // add new to-do 
 function addNewTodo() {
    
-   // create elements
+   if (!userInputTodo.value) {
+     alert("todo is empty");
+   } else {
+      // create elements
     let newTodo = document.createElement("div");
     let newTodoItemColor = document.createElement("div");
     let newTodoI = document.createElement("i");
@@ -119,37 +79,76 @@ function addNewTodo() {
     // asign values  
     newTodoP.innerText = userInputTodo.value;
     newTodoItemColor.style.background = userInputColor.value; 
+    newTodoI.style.color = userInputColor.value;
     
     // append in 
     newTodo.append(newTodoItemColor);
     newTodo.append(newTodoI);
     newTodo.append(newTodoP);
-    todoPage.append(newTodo);
+    
+    let blurDiv = document.querySelector("#shadow-onbottom-Todo");
+    allTodos.append(newTodo);
+    
+    
+   closeTodoPopup();
+   saveTodoData();
+   }
+}
+
+function changeTodoState(event) {
+  
+   let todoIconS = event.target.querySelector("i");
+   let todoP = event.target.querySelector("p");
+   let itemColorbox = event.target.querySelector(".item-color");
    
-   closeNewFolderWindow(); 
+  let itemColor = window.getComputedStyle(itemColorbox).backgroundColor;
+  
+
+if (todoIconS.classList.contains("fa-square")) {
+   todoIconS.setAttribute("class","fas fa-check-square");
+     todoIconS.style.color = "green";
+     todoP.style.textDecoration = "line-through";
+     todoP.style.color = "green";
+     
+} else if (todoIconS.classList.contains("fa-check-square")) {
+   todoIconS.setAttribute("class","fas fa-square");
+     todoIconS.style.color = itemColor;
+     todoP.style.textDecoration = "none";
+     todoP.style.color = "#000";
+    
+    
+}
+saveTodoData();
+}
+
+
+function saveTodoData() {
+localStorage.setItem("todoData",`${allTodos.innerHTML}`)
+}
+
+function displaySavedData() {
+  todoData = localStorage.getItem("todoData");
+  if (todoData === undefined) {
+    console.log("undefined")
+  } else {
+    allTodos.innerHTML = todoData;
+  }
 }
 
 
 // +++++>   Event listener   <+++++
-
 // open close window popup
-addNewFolderBtn.addEventListener("click",openNewFolderWindow);
-popupCloseBtn.addEventListener("click",closeNewFolderWindow);
-todoCloseBtn.addEventListener("click",closeNewFolderWindow);
+addNewFolderBtn.addEventListener("click",openTodoPopup);
 
-// go back into folders page e->h
-goBackBtn.addEventListener("click",closeTodoPage);
-
-// add new folder event->handle
-popupAddBtn.addEventListener("click",addNewFolder);
+todoCloseBtn.addEventListener("click",closeTodoPopup);
 
 todoAddBtn.addEventListener("click",addNewTodo); 
 
 // **> detect click on folder  <**
-document.querySelector(".folder-page").addEventListener("click",(event) =>{
+document.querySelector(".todo-page").addEventListener("click",(event) =>{
   
-  if (event.target.classList.contains("folder")) {
-    openTodoPage(event);
+  if (event.target.classList.contains("todo")) {
+    changeTodoState(event);
   } 
 })
 
